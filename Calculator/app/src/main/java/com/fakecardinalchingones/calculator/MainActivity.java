eab614775d634e55b597c5edd5216c11fedbf843
willriot <cybertitans01@gmail.com>
2016-01-23 18:14:25 -0800

    Completed Calculator App (No Debugging Done)
    If there are no inputs then the app crashes.

--- Calculator/app/src/main/java/com/fakecardinalchingones/calculator/MainActivity.java
+++ Calculator/app/src/main/java/com/fakecardinalchingones/calculator/MainActivity.java
@@ -8,14 +8,81 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText operand1;
+    private EditText operand2;
+    private Button btnAddition;
+    private Button btnSubtraction;
+    private Button btnMultiplication;
+    private Button btnDivision;
+    private TextView txtResult;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+
+        operand1 = (EditText) findViewById(R.id.editText);
+        operand2 = (EditText) findViewById(R.id.editText2);
+
+        btnAddition = (Button) findViewById(R.id.btnAddition);
+        btnSubtraction = (Button) findViewById(R.id.btnSubtraction);
+        btnMultiplication = (Button) findViewById(R.id.btnMultiplication);
+        btnDivision = (Button) findViewById(R.id.btnDivision);
+        txtResult = (TextView) findViewById(R.id.txtResult);
+
+
+        btnAddition.setOnClickListener(new View.OnClickListener() {
+                                           @Override
+                                           public void onClick(View v) {
+                                               double op1 = Double.parseDouble(operand1.getText().toString());
+                                               double op2 = Double.parseDouble(operand2.getText().toString());
+
+                                               double theResult = op1 + op2;
+                                               txtResult.setText(Double.toString(theResult));
+                                           }
+                                       });
+        btnSubtraction.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double op1 = Double.parseDouble(operand1.getText().toString());
+                double op2 = Double.parseDouble(operand2.getText().toString());
+
+                double theResult = op1 - op2;
+                txtResult.setText(Double.toString(theResult));
+            }
+        });
+
+        btnMultiplication.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double op1 = Double.parseDouble(operand1.getText().toString());
+                double op2 = Double.parseDouble(operand2.getText().toString());
+
+                double theResult = op1 * op2;
+                txtResult.setText(Double.toString(theResult));
+            }
+        });
+
+        btnDivision.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double op1 = Double.parseDouble(operand1.getText().toString());
+                double op2 = Double.parseDouble(operand2.getText().toString());
+
+                double theResult = op1 / op2;
+                txtResult.setText(Double.toString(theResult));
+            }
+        });
+
+                Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
     }
